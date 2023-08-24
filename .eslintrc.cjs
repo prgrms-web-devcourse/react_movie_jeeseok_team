724e3f53862a2f10b5385b33a6a03717b6f3e479
seeyoujeong <chan3358@naver.com>
2023-08-24 13:28:22 +0900

    feat: prettier가 포함된 초기 세팅

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
@@ -11,4 +11,4 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
-}
+};
